51f886559ea03a4ba248785769d9ebbc95f63771
Narin Sundarabhaya <nrs710@gmail.com>
2017-06-04 23:21:15 -0700

    set up array/object

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,12 +10,40 @@ On Page Load:
 
  */
 
+var trains = {
+    data: [
+        burlington = {
+            destination: "burlington",
+            time: "06:00",
+            frequency: 60,
+        }
+    ]
+}
+
+// function to display each trains in the table
+var renderPage = function() {
+
+    for (i = 0; i < trains.data.length; i++) {
+
+    }
+
+}
+
 // event handler for storing train values
 $("#btn-add").on("click", function() {
-    var $trainName = $("#train-name").val().trim();
-    var $trainDestination = $("#train-destination").val().trim();
-    var $trainTime = $("#train-time").val().trim();
-    var $trainFrequency = $("#time-freq").val().trim();
+    var trainName = $("#train-name").val().trim();
+    var trainDestination = $("#train-destination").val().trim();
+    var trainTime = $("#train-time").val().trim();
+    var trainFrequency = $("#time-freq").val().trim();
+    // create object & add values from user input
+    var trainObj = {};
+    trainObj.name = trainName;
+    trainObj.destination = trainDestination;
+    trainObj.time = trainTime;
+    trainObj.frequency = trainFrequency;
+    // add to global trains array
+    trains.data.push(trainObj);
 
-    console.log("name: " + $trainName + " destination: " + $trainDestination + " time: " + $trainTime + " frequency: " + $trainFrequency);
 });
+
+console.log(trains);
