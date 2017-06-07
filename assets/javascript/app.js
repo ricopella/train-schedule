a30da78dd77ee5ebe27f8b2b64b9d97dffe9c19f
Narin Sundarabhaya <nrs710@gmail.com>
2017-06-06 23:01:15 -0700

    updated functionality of adding train to firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,10 +14,6 @@ On Page Load:
 
  */
 
-// global variables
-var trains = {
-    data: []
-}
 
 // Initialize Firebase
 var config = {
@@ -33,45 +29,59 @@ firebase.initializeApp(config);
 // Assign the reference to the database to a variable named 'database'
 var database = firebase.database();
 
-// function to display each train in the table
-var renderPage = function() {
 
-    for (i = 0; i < trains.data.length; i++) {
+database.ref().on("value", function(snapshot) {
+
+    // If firebase has train stored
+    if (snapshot.child("trainObject").exists()) {
+
+        // function to display each train in the table
+
         $("tbody").append(
-            "<tr><td>" + trains.data[i].name + "</td>" +
-            "<td>" + trains.data[i].destination + "</td>" +
-            "<td>" + trains.data[i].time + "</td>" +
-            "<td>" + trains.data[i].frequency + "</td>" + "<hr /></tr>"
+            "<tr><td>" + snapshot.val().name + "</td>" +
+            "<td>" + snapshot.val().destination + "</td>" +
+            "<td>" + snapshot.val().time + "</td>" +
+            "<td>" + snapshot.val().frequency + "</td><hr /></tr>"
         )
-    }
-    console.log("render page: " + trains.data);
-};
+    } else {
+        $("#table-data").html("<h2>No Current Trains Scheduled</h2>");
+    };
 
-// event handler for storing train values
-$("#btn-add").on("click", function() {
+}, function(errorObject) {
+    console.log("The read failed: " + errorObject.code);
+});
+
+// function to call the button event, and store the values in the input form
+var storeTrain = function(event) {
+
+    event.preventDefault();
+
+    // get & store input values
     var trainName = $("#train-name").val().trim();
     var trainDestination = $("#train-destination").val().trim();
     var trainTime = $("#train-time").val().trim();
     var trainFrequency = $("#time-freq").val().trim();
-    // create object & add values from user input
+
     var trainObj = {};
     trainObj.name = trainName;
     trainObj.destination = trainDestination;
     trainObj.time = trainTime;
     trainObj.frequency = trainFrequency;
-    // add to global trains array
-    trains.data.push(trainObj);
-    // update HTML on submit
-    renderPage();
 
-    $("#train-name").empty();
-    $("#train-destination").empty();
-    $("#train-time").empty();
-    $("#time-freq").empty();
-    console.log("button click: " + trains.data);
+    // add to firebase databse
+    database.ref().set({
+        trainObject: trainObj,
+    });
+    console.log("this should work3");
+
+
+    //  empty for once submitted
+    $("#train-name").clear();
+    $("#train-destination").clear();
+    $("#train-time").clear();
+    $("#time-freq").clear();
+};
+
+$(document).ready(function() {
+    $("#btn-add").on("click", storeTrain);
 });
-
-console.log("global: " + trains.data);
-
-// on page load, render trains in localStorage
-renderPage();
